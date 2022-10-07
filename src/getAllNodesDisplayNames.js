c0dcc6c8505650f00dfb1790f670b4c95aa74449
Iván Ovejero <ivov.src@gmail.com>
2022-10-07 20:11:08 +0200

    Add `typescript` to install

--- src/getAllNodesDisplayNames.js
+++ src/getAllNodesDisplayNames.js
@@ -7,7 +7,9 @@ const { PARSER_CONTENT } = require("./constants");
  * @returns { Promise<string[]> } e.g. ["Action Network", "Active Campaign", etc.]
  */
 async function getAllNodesDisplayNames() {
-  await exec(`touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`);
+  await exec(
+    `npm i typescript; touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`
+  );
   const result = await exec("npx ts-node parser.ts");
 
   return JSON.parse(result.stdout);
