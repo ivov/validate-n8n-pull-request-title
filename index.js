44767d4ba78246feb7a002d041f603308eef06a7
Iván Ovejero <ivov.src@gmail.com>
2022-10-07 15:37:51 +0200

    Change dep name

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ async function run() {
 
     // build /nodes-base so displayNames can be fetched
     if (/\(.* Node\)/.test(pullRequest.title)) {
-      await exec("cd packages/nodes-base; npm i tsc; tsc");
+      // await exec("cd packages/nodes-base; npm i tsc; tsc");
+      await exec("cd packages/nodes-base; npm i typescript; tsc");
     }
 
     const issues = validatePrTitle(pullRequest.title);
