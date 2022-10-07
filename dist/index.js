f7955d06f2d9adfd86dc27798523c7144a995ce7
Iván Ovejero <ivov.src@gmail.com>
2022-10-07 20:09:50 +0200

    Remove deps installation step

--- dist/index.js
+++ dist/index.js
@@ -10340,7 +10340,6 @@ const { PARSER_CONTENT } = __nccwpck_require__(4438);
  * @returns { Promise<string[]> } e.g. ["Action Network", "Active Campaign", etc.]
  */
 async function getAllNodesDisplayNames() {
-  await exec("npm i typescript fast-glob");
   await exec(`touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`);
   const result = await exec("npx ts-node parser.ts");
   console.log("result", result.stdout);
@@ -10351,40 +10350,6 @@ async function getAllNodesDisplayNames() {
 module.exports = {
   getAllNodesDisplayNames,
 };
-
-// const path = require("path");
-
-// /**
-//  * @returns ["Action Network", "Active Campaign", etc.]
-//  */
-// function getAllNodesDisplayNames() {
-//   return getAllNodesDistPaths().map((distPath) => {
-//     const nodeFilePath = path.resolve("packages", "nodes-base", distPath);
-//     const NodeClass = getNodeClass(nodeFilePath);
-//     const node = new NodeClass();
-
-//     return node.description.displayName;
-//   });
-// }
-
-// /**
-//  * @returns ["dist/nodes/ActionNetwork/ActionNetwork.node.js", etc.]
-//  */
-// function getAllNodesDistPaths() {
-//   const nodesBasePackageJsonPath = path.resolve(
-//     "packages",
-//     "nodes-base",
-//     "package.json"
-//   );
-
-//   return require(nodesBasePackageJsonPath).n8n.nodes;
-// }
-
-// function getNodeClass(nodeFilePath) {
-//   const className = nodeFilePath.split("/").pop().replace(".node.js", "");
-
-//   return require(nodeFilePath)[className];
-// }
 
 
 /***/ }),
--- src/getAllNodesDisplayNames.js
+++ src/getAllNodesDisplayNames.js
@@ -7,10 +7,8 @@ const { PARSER_CONTENT } = require("./constants");
  * @returns { Promise<string[]> } e.g. ["Action Network", "Active Campaign", etc.]
  */
 async function getAllNodesDisplayNames() {
-  await exec("npm i typescript fast-glob");
   await exec(`touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`);
   const result = await exec("npx ts-node parser.ts");
-  console.log("result", result.stdout);
 
   return JSON.parse(result.stdout);
 }
@@ -18,37 +16,3 @@ async function getAllNodesDisplayNames() {
 module.exports = {
   getAllNodesDisplayNames,
 };
-
-// const path = require("path");
-
-// /**
-//  * @returns ["Action Network", "Active Campaign", etc.]
-//  */
-// function getAllNodesDisplayNames() {
-//   return getAllNodesDistPaths().map((distPath) => {
-//     const nodeFilePath = path.resolve("packages", "nodes-base", distPath);
-//     const NodeClass = getNodeClass(nodeFilePath);
-//     const node = new NodeClass();
-
-//     return node.description.displayName;
-//   });
-// }
-
-// /**
-//  * @returns ["dist/nodes/ActionNetwork/ActionNetwork.node.js", etc.]
-//  */
-// function getAllNodesDistPaths() {
-//   const nodesBasePackageJsonPath = path.resolve(
-//     "packages",
-//     "nodes-base",
-//     "package.json"
-//   );
-
-//   return require(nodesBasePackageJsonPath).n8n.nodes;
-// }
-
-// function getNodeClass(nodeFilePath) {
-//   const className = nodeFilePath.split("/").pop().replace(".node.js", "");
-
-//   return require(nodeFilePath)[className];
-// }
