1d3a9c58f2aa5f539207f2645b27d19f26a756df
Iván Ovejero <ivov.src@gmail.com>
2022-10-12 10:35:22 +0200

    Render types and scopes in markdown

--- src/constants.js
+++ src/constants.js
@@ -11,19 +11,22 @@ const TYPES = [
 
 const SCOPES = ["API", "core", "editor"];
 
+const displayTypes = TYPES.map((type) => `\`${type}\``).join(", ");
+const displayScopes = SCOPES.map((type) => `\`${type}\``).join(", ");
+
 const NO_CHANGELOG = "(no-changelog)";
 
 const ERRORS = {
   CONVENTIONAL_SCHEMA_MISMATCH: [
     "PR title does not match PR title convention: type: subject or type(scope): subject",
-    `For \`type\`, use \`type:\` or \`type(scope):\`, where \`type\` is one of {${TYPES}}`,
-    `If present, \`scope\` must be one of {${SCOPES}} or \`<displayName> Node\``,
+    `For \`type\`, use \`type:\` or \`type(scope):\`, where \`type\` is one of ${displayTypes}`,
+    `If present, \`scope\` must be one of ${displayScopes} or \`<displayName> Node\``,
     "For the subject, mind the whitespace, start with lowercase and omit final period",
   ].join(". "),
   TICKET_NUMBER_PRESENT: "PR title must not contain a ticket number",
-  TYPE_NOT_FOUND: `Failed to find \`type\` in PR title. Expected one of {${TYPES}}`,
-  INVALID_TYPE: `Unknown \`type\` in PR title. Expected one of {${TYPES}}`,
-  INVALID_SCOPE: `Unknown \`scope\` in PR title. Expected one of {${SCOPES}} or \`<displayName> Node\``,
+  TYPE_NOT_FOUND: `Failed to find \`type\` in PR title. Expected one of ${displayTypes}`,
+  INVALID_TYPE: `Unknown \`type\` in PR title. Expected one of ${displayTypes}`,
+  INVALID_SCOPE: `Unknown \`scope\` in PR title. Expected one of ${displayScopes} or \`<displayName> Node\``,
   UPPERCASE_INITIAL_IN_SUBJECT: "First char of subject must be lowercase",
   FINAL_PERIOD_IN_SUBJECT: "Subject must not end with a period",
   NO_PRESENT_TENSE_IN_SUBJECT: "Subject must use present tense",
