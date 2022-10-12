1e306a869f6445b1a9e77b1699fb9c8f306b8ee4
Iván Ovejero <ivov.src@gmail.com>
2022-10-12 12:43:04 +0200

    Add logging

--- src/parserContent.js
+++ src/parserContent.js
@@ -13,11 +13,11 @@ import ts from 'typescript';
 const NODES_DIR = path.resolve('packages', 'nodes-base', 'nodes');
 
 async function getDisplayNames() {
+	console.log(path.resolve(NODES_DIR, '**', '*.node.ts'));
 	const [nodeFilepaths, versionDescriptionFilepaths] = await Promise.all([
 		glob(path.resolve(NODES_DIR, '**', '*.node.ts')),
 		glob(path.resolve(NODES_DIR, '**', 'versionDescription.ts')),
 	]);
-	console.log(nodeFilepaths);
 
 	const nodeFiles = nodeFilepaths.reduce<string[]>((acc, cur) => {
 		let displayName = fromMajorityNodeFile(cur);
